d4e43e93e8de917a3d62549ac1ec07a8416645b4
John Roca <temoty.roca@goabroad.com>
2016-03-30 17:59:38 +0800

    Added Filters and Array of books

--- js/app.js
+++ js/app.js
@@ -4,4 +4,24 @@ BookStore
 	.controller('BookController', ['$scope', function($scope) {
 
 		$scope.appTitle = "Book Store Application";
+
+		$scope.books = [
+			{ 
+				title  : 'The Life-Changing Magic of Tidying Up: The Japanese Art of Decluttering and Organizing', 
+				author : 'Marie Kondo', 
+				price  : '9.68' 
+			},
+			{ 
+				title  : 'Quieting Your Heart: 6-Month Bible-Study Journal', 
+				author : 'Darlene Schacht', 
+				price  : '7.14' 
+			},
+			{ 
+				title  : 'First 100 Words', 
+				author : 'Roger Priddy', 
+				price  : '3.19' 
+			},
+		];
+
+		
 	}]);
