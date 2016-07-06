261e6876c2fa515f657df63c42b53a65caee9db0
John Roca <rocajohntemoty@gmail.com>
2016-07-06 12:06:06 +0800

    Code Update

---
+++ js/app.js
@@ -0,0 +1,37 @@
+var BookStore = angular.module('BookStore', []);
+
+BookStore.controller('BookController', function($scope){
+	$scope.books = [
+		{ 
+			title  : 'The Life-Changing Magic of Tidying Up: The Japanese Art of Decluttering and Organizing', 
+			author : 'Marie Kondo', 
+			price  : '9.68' 
+		},
+		{ 
+			title  : 'First 100 Words', 
+			author : 'Roger Priddy', 
+			price  : '3.19' 
+		},
+		{ 
+			title  : 'Quieting Your Heart: 6-Month Bible-Study Journal', 
+			author : 'Darlene Schacht', 
+			price  : '7.14' 
+		},
+	];
+
+	$scope.save = function() {
+		$scope.books.push({
+			title : $scope.title,
+			author: $scope.author,
+			price : $scope.price
+		});
+
+		$scope.reset();
+	};
+
+	$scope.reset = function() {
+		$scope.title = '';
+		$scope.author = '';
+		$scope.price = '';
+	};
+});
